1bc4f807927ca60fd443587879648488f66525ed
Alison Turner <alisonmoons@gmail.com>
2022-03-04 10:49:21 -0400

    Added on hover player names

--- src/main/java/Starlight/characters/Asphodene.java
+++ src/main/java/Starlight/characters/Asphodene.java
@@ -3,6 +3,7 @@ package Starlight.characters;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 
 public class Asphodene extends Starfarers {
@@ -14,6 +15,7 @@ public class Asphodene extends Starfarers {
         linkIntegrals(eridani);
         movePosition(drawX+100f,drawY);
         eridani.movePosition(eridani.drawX-100f, eridani.drawY);
+        this.name = NAMES[2];
     }
 
     @Override
@@ -25,5 +27,6 @@ public class Asphodene extends Starfarers {
     public void preBattlePrep() {
         super.preBattlePrep();
         AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(this, this, new StrengthPower(this, 1)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(otherPlayerReference, otherPlayerReference, new DexterityPower(otherPlayerReference, 1)));
     }
 }
--- src/main/java/Starlight/characters/Eridani.java
+++ src/main/java/Starlight/characters/Eridani.java
@@ -1,18 +1,9 @@
 package Starlight.characters;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.DexterityPower;
-
 public class Eridani extends Starfarers {
     public Eridani(String name, PlayerClass setClass) {
         super(name, setClass);
         playAnimation("idleEridani");
-    }
-
-    @Override
-    public void preBattlePrep() {
-        super.preBattlePrep();
-        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(this, this, new DexterityPower(this, 1)));
+        this.name = NAMES[3];
     }
 }
