82fd57997f065ccaa9fa055085838f56f74adca3
Alison Turner <alisonmoons@gmail.com>
2022-08-19 12:22:34 -0300

    Add Spell Power

---
+++ src/main/java/Starlight/powers/SpellPower.java
@@ -0,0 +1,62 @@
+package Starlight.powers;
+
+import Starlight.TheStarlightMod;
+import Starlight.cards.abstracts.AbstractMagickCard;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class SpellPower extends AbstractPower {
+
+    public static final String POWER_ID = TheStarlightMod.makeID(SpellPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public SpellPower(AbstractCreature owner, int amount) {
+        this.ID = POWER_ID;
+        this.name = NAME;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.loadRegion("book");
+        updateDescription();
+    }
+
+    @Override
+    public float atDamageGive(float damage, DamageInfo.DamageType type, AbstractCard card) {
+        if (card instanceof AbstractMagickCard) {
+            return damage + amount;
+        }
+        return damage;
+    }
+
+    @Override
+    public float modifyBlock(float blockAmount, AbstractCard card) {
+        if (card instanceof AbstractMagickCard) {
+            return blockAmount + amount;
+        }
+        return amount;
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card instanceof AbstractMagickCard && !card.purgeOnUse && (card.baseDamage != -1 || card.baseBlock != -1)) {
+            flash();
+            this.addToBot(new ReducePowerAction(owner, owner, this, amount));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+}
