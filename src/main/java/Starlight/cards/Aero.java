f11c7a8df2c9dc1f18c5829c741a609de2f6e6d4
Alison Turner <alisonmoons@gmail.com>
2022-08-19 12:22:07 -0300

    Add Aero and Thunder

---
+++ src/main/java/Starlight/cards/Aero.java
@@ -0,0 +1,48 @@
+package Starlight.cards;
+
+import Starlight.cards.abstracts.AbstractMagickCard;
+import Starlight.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Starlight.TheStarlightMod.makeID;
+
+public class Aero extends AbstractMagickCard {
+    public final static String ID = makeID(Aero.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+
+    private static final int COST = 1;
+    private static final int DMG = 7;
+    private static final int UP_DMG = 3;
+    private static final int EFFECT = 2;
+    private static final int UP_EFFECT = 1;
+
+    public Aero() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        baseDamage = damage = DMG;
+        baseMagicNumber = magicNumber = EFFECT;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
+        Wiz.atb(new DrawCardAction(magicNumber));
+    }
+
+    @Override
+    protected void upgradeName() {
+        ++this.timesUpgraded;
+        this.upgraded = true;
+        this.name = cardStrings.EXTENDED_DESCRIPTION[0];
+        this.initializeTitle();
+    }
+
+    public void upp() {
+        upgradeDamage(UP_DMG);
+        //upgradeMagicNumber(UP_EFFECT);
+    }
+}
---
+++ src/main/java/Starlight/cards/Thunder.java
@@ -0,0 +1,52 @@
+package Starlight.cards;
+
+import Starlight.cards.abstracts.AbstractMagickCard;
+import Starlight.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
+
+import static Starlight.TheStarlightMod.makeID;
+
+public class Thunder extends AbstractMagickCard {
+    public final static String ID = makeID(Thunder.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+
+    private static final int COST = 1;
+    private static final int DMG = 4;
+    private static final int UP_DMG = 1;
+    private static final int EFFECT = 1;
+    private static final int UP_EFFECT = 1;
+
+    public Thunder() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        baseDamage = damage = DMG;
+        baseMagicNumber = magicNumber = EFFECT;
+        isMultiDamage = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.forAllMonstersLiving(mon -> new VFXAction(new LightningEffect(mon.drawX, mon.drawY), 0.05F));
+        allDmg(AbstractGameAction.AttackEffect.NONE);
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(m, new VulnerablePower(mon, magicNumber, false)));
+    }
+
+    @Override
+    protected void upgradeName() {
+        ++this.timesUpgraded;
+        this.upgraded = true;
+        this.name = cardStrings.EXTENDED_DESCRIPTION[0];
+        this.initializeTitle();
+    }
+
+    public void upp() {
+        upgradeDamage(UP_DMG);
+        upgradeMagicNumber(UP_EFFECT);
+    }
+}
