d68568c8cf2b891e8fffcadc9eae8e78c92701eb
Alison Turner <alisonmoons@gmail.com>
2022-09-03 17:30:34 -0300

    Thunder now single target. Increased Cost, damage, and Vuln

--- src/main/java/Starlight/cards/Thunder.java
+++ src/main/java/Starlight/cards/Thunder.java
@@ -17,27 +17,29 @@ public class Thunder extends AbstractMagickCard {
     public final static String ID = makeID(Thunder.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
-    private static final int COST = 1;
-    private static final int DMG = 4;
-    private static final int UP_DMG = 1;
-    private static final int EFFECT = 1;
+    private static final int COST = 2;
+    private static final int DMG = 12;
+    private static final int UP_DMG = 2;
+    private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
 
     public Thunder() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
         baseMagicNumber = magicNumber = EFFECT;
-        isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.atb(new SFXAction("THUNDERCLAP", 0.05F));
-        Wiz.forAllMonstersLiving(mon -> Wiz.atb(new VFXAction(new LightningEffect(mon.drawX, mon.drawY), 0.05F)));
+        Wiz.atb(new VFXAction(new LightningEffect(m.drawX, m.drawY), 0.05F));
+        dmg(m, AbstractGameAction.AttackEffect.NONE);
+        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        /*Wiz.forAllMonstersLiving(mon -> Wiz.atb(new VFXAction(new LightningEffect(mon.drawX, mon.drawY), 0.05F)));
         allDmg(AbstractGameAction.AttackEffect.NONE);
-        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new VulnerablePower(mon, magicNumber, false)));
+        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new VulnerablePower(mon, magicNumber, false)));*/
     }
 
     @Override
