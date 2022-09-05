27cfa87aaceb7ff118eeeba0276a6109888cce35
Alison Turner <alisonmoons@gmail.com>
2022-09-04 23:09:12 -0300

    Buff Magic Wand SP 4 -> 5

--- src/main/java/Starlight/relics/MagicWand.java
+++ src/main/java/Starlight/relics/MagicWand.java
@@ -2,24 +2,15 @@ package Starlight.relics;
 
 import Starlight.characters.StarlightSisters;
 import Starlight.powers.SpellPower;
-import Starlight.relics.Campfire.ConstellationOption;
 import Starlight.util.Wiz;
-import basemod.abstracts.CustomSavable;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
-import com.megacrit.cardcrawl.powers.DexterityPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
-
-import java.util.ArrayList;
 
 import static Starlight.TheStarlightMod.makeID;
 
 public class MagicWand extends AbstractEasyRelic {
     public static final String ID = makeID(MagicWand.class.getSimpleName());
-    private static final int SP = 4;
+    private static final int SP = 5;
 
     public MagicWand() {
         super(ID, RelicTier.STARTER, LandingSound.MAGICAL, StarlightSisters.Enums.METEORITE_PURPLE_COLOR);
