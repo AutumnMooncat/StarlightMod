ea1310940be1b53db6e332cdfbeb2446be0ba65b
Alison Turner <alisonmoons@gmail.com>
2022-09-08 18:33:34 -0300

    The Plot now always costs 1, but draws 2(3) cards first

--- src/main/java/Starlight/cards/ThePlot.java
+++ src/main/java/Starlight/cards/ThePlot.java
@@ -1,16 +1,13 @@
 package Starlight.cards;
 
 import Starlight.actions.ForetellAction;
-import Starlight.actions.ScryFollowUpAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
 import Starlight.cards.interfaces.TagTeamCard;
-import Starlight.powers.ProvidencePower;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.Tactician;
-import com.megacrit.cardcrawl.cards.purple.EmptyFist;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -25,22 +22,26 @@ public class ThePlot extends AbstractEasyCard implements TagTeamCard {
 
     private static final int COST = 1;
     private static final int UP_COST = 0;
+    private static final int DRAW = 2;
+    private static final int UP_DRAW = 1;
 
     private boolean tagged = false;
 
     public ThePlot() {
         super(ID, COST, TYPE, RARITY, TARGET);
+        baseMagicNumber = magicNumber = DRAW;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         tagged = false;
+        Wiz.atb(new DrawCardAction(magicNumber));
         Wiz.atb(new ForetellAction(p.hand, 1, c -> true, new AbstractGameAction() {
             @Override
             public void update() {
                 for (AbstractCard card : ForetellAction.foretoldCards) {
-                    if (card.canUpgrade()) {
+                    /*if (card.canUpgrade()) {
                         card.upgrade();
-                    }
+                    }*/
                     if (tagged) {
                         card.freeToPlayOnce = true;
                     }
@@ -51,7 +52,8 @@ public class ThePlot extends AbstractEasyCard implements TagTeamCard {
     }
 
     public void upp() {
-        upgradeBaseCost(UP_COST);
+        //upgradeBaseCost(UP_COST);
+        upgradeMagicNumber(UP_DRAW);
     }
 
     @Override
