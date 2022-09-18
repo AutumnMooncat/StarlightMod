738355cde8788ccdb237c80753519710eaf5f54e
Alison Turner <alisonmoons@gmail.com>
2022-09-18 17:18:17 -0300

    Shuffle Strike should now discard if the hand is full

--- src/main/java/Starlight/actions/MoveFromDrawToHandAction.java
+++ src/main/java/Starlight/actions/MoveFromDrawToHandAction.java
@@ -17,19 +17,24 @@ public class MoveFromDrawToHandAction extends AbstractGameAction {
 
     public void update() {
         if (this.duration == Settings.ACTION_DUR_FAST) {
-            if (AbstractDungeon.player.drawPile.contains(this.card) && AbstractDungeon.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
-                AbstractDungeon.player.hand.addToHand(this.card);
-                this.card.unhover();
-                this.card.setAngle(0.0F, true);
-                this.card.lighten(false);
-                this.card.drawScale = 0.12F;
-                this.card.targetDrawScale = 0.75F;
-                this.card.applyPowers();
-                AbstractDungeon.player.drawPile.removeCard(this.card);
-            }
+            if (AbstractDungeon.player.drawPile.contains(this.card)) {
+                if (AbstractDungeon.player.hand.size() < BaseMod.MAX_HAND_SIZE) {
+                    AbstractDungeon.player.hand.addToHand(this.card);
+                    this.card.unhover();
+                    this.card.setAngle(0.0F, true);
+                    this.card.lighten(false);
+                    this.card.drawScale = 0.12F;
+                    this.card.targetDrawScale = 0.75F;
+                    this.card.applyPowers();
+                    AbstractDungeon.player.drawPile.removeCard(this.card);
+                    AbstractDungeon.player.hand.refreshHandLayout();
+                    AbstractDungeon.player.hand.glowCheck();
+                } else {
+                    AbstractDungeon.player.createHandIsFullDialog();
+                    AbstractDungeon.player.drawPile.moveToDiscardPile(this.card);
+                }
 
-            AbstractDungeon.player.hand.refreshHandLayout();
-            AbstractDungeon.player.hand.glowCheck();
+            }
         }
 
         this.tickDuration();
