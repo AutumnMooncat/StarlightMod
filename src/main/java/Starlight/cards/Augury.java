df96d8ad94de953ea8e3b1231a53d94613f5f1e4
Alison Turner <alisonmoons@gmail.com>
2022-09-20 22:18:48 -0300

    Augury damage nerf 9(12) -> 7(8). Now draws and Upgrades 1(2) cards.

--- src/main/java/Starlight/cards/Augury.java
+++ src/main/java/Starlight/cards/Augury.java
@@ -1,19 +1,18 @@
 package Starlight.cards;
 
-import Starlight.actions.ForetellAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.cards.interfaces.TagTeamCard;
+import Starlight.util.CardArtRoller;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.purple.FlyingSleeves;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.purple.JustLucky;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Starlight.TheStarlightMod.makeID;
 
-public class Augury extends AbstractEasyCard implements TagTeamCard {
+public class Augury extends AbstractEasyCard {
     public final static String ID = makeID(Augury.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
@@ -21,8 +20,8 @@ public class Augury extends AbstractEasyCard implements TagTeamCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
-    private static final int DMG = 9;
-    private static final int UP_DMG = 3;
+    private static final int DMG = 7;
+    private static final int UP_DMG = 1;
     private static final int DRAW = 1;
     private static final int UP_DRAW = 1;
 
@@ -34,11 +33,22 @@ public class Augury extends AbstractEasyCard implements TagTeamCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SMASH);
-        Wiz.atb(new DrawCardAction(magicNumber));
+        Wiz.atb(new DrawCardAction(magicNumber, new AbstractGameAction() {
+            @Override
+            public void update() {
+                for (AbstractCard c : DrawCardAction.drawnCards) {
+                    if (c.canUpgrade()) {
+                        c.upgrade();
+                    }
+                }
+                this.isDone = true;
+            }
+        }));
     }
 
     public void upp() {
         upgradeDamage(UP_DMG);
+        upgradeMagicNumber(UP_DRAW);
     }
 
     @Override
@@ -46,8 +56,18 @@ public class Augury extends AbstractEasyCard implements TagTeamCard {
         return JustLucky.ID;
     }
 
-    @Override
+/*    @Override
     public void onTagTrigger(AbstractPlayer p, AbstractMonster m) {
         Wiz.atb(new ForetellAction(p.hand));
+    }*/
+
+    /*@Override
+    public void onLunaTrigger(AbstractPlayer p, AbstractMonster m) {
+        Wiz.atb(new DrawCardAction(magicNumber));
+    }*/
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.35f, 0.5f, 0.5f, 0.5f, false);
     }
 }
