1d11cef0f380d80efd893cb8034686c70bbfbd3a
Alison Turner <alisonmoons@gmail.com>
2022-09-21 13:38:44 -0300

    Add OnSwapCard interface

--- src/main/java/Starlight/actions/SwapAction.java
+++ src/main/java/Starlight/actions/SwapAction.java
@@ -1,11 +1,17 @@
 package Starlight.actions;
 
+import Starlight.cards.interfaces.OnSwapCard;
+import Starlight.cards.interfaces.OnTagTeamTriggeredCard;
 import Starlight.characters.StarlightSisters;
 import Starlight.powers.TagTeamPower;
 import Starlight.powers.interfaces.OnSwapPower;
+import Starlight.ui.ProjectedCardManager;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import java.util.ArrayList;
 
 public class SwapAction extends AbstractGameAction {
 
@@ -30,6 +36,19 @@ public class SwapAction extends AbstractGameAction {
             for (AbstractPower p : sisters.powers) {
                 if (p instanceof OnSwapPower) {
                     ((OnSwapPower) p).onSwap(sisters.attackerInFront);
+                }
+            }
+            ArrayList<AbstractCard> cards = new ArrayList<>();
+            cards.addAll(sisters.hand.group);
+            cards.addAll(sisters.drawPile.group);
+            cards.addAll(sisters.discardPile.group);
+            cards.addAll(sisters.exhaustPile.group);
+            cards.addAll(sisters.limbo.group);
+            cards.addAll(ProjectedCardManager.cards.group);
+
+            for (AbstractCard c : cards) {
+                if (c instanceof OnSwapCard) {
+                    ((OnSwapCard) c).onSwap(sisters.attackerInFront);
                 }
             }
         }
