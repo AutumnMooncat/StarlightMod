51f297a00a0d099d580a67f0fe429fbcd2f37d47
Alison Turner <alisonmoons@gmail.com>
2022-09-22 12:39:54 -0300

    Grit now exhausts the played card

---
+++ src/main/java/Starlight/cards/status/Grit.java
@@ -0,0 +1,48 @@
+package Starlight.cards.status;
+
+import Starlight.actions.PlayRandomCardAction;
+import Starlight.cards.abstracts.AbstractEasyCard;
+import Starlight.cards.abstracts.AbstractMagickCard;
+import Starlight.util.CardArtRoller;
+import Starlight.util.Wiz;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Starlight.TheStarlightMod.makeID;
+
+public class Grit extends AbstractEasyCard {
+    public final static String ID = makeID(Grit.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardType TYPE = CardType.STATUS;
+
+    private static final int COST = 1;
+
+    public Grit() {
+        super(ID, COST, TYPE, RARITY, TARGET, CardColor.COLORLESS);
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        Wiz.atb(new PlayRandomCardAction(p.drawPile, 1, c -> true, cards -> {
+            for (AbstractCard c : cards) {
+                c.exhaustOnUseOnce = true;
+            }
+        }));
+    }
+
+    public void upp() {}
+
+    @Override
+    public String cardArtCopy() {
+        return VoidCard.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.8f, 0.5f, 0.5f, 0.5f, false);
+    }
+}
