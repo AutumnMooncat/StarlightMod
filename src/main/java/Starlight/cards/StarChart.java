ec78819a4eb027a3b4017fb0334875a33efa87a3
Alison Turner <alisonmoons@gmail.com>
2022-09-24 00:47:41 -0300

    Rework Deduce to Star Chart

--- src/main/java/Starlight/cards/StarChart.java
+++ src/main/java/Starlight/cards/StarChart.java
@@ -1,17 +1,21 @@
 package Starlight.cards;
 
 import Starlight.cards.abstracts.AbstractEasyCard;
+import Starlight.damageMods.ResonantDamage;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import basemod.helpers.TooltipInfo;
 import com.megacrit.cardcrawl.cards.purple.Evaluate;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static Starlight.TheStarlightMod.makeID;
 
-public class Deduce extends AbstractEasyCard {
-    public final static String ID = makeID(Deduce.class.getSimpleName());
+public class StarChart extends AbstractEasyCard {
+    public final static String ID = makeID(StarChart.class.getSimpleName());
+    private static ArrayList<TooltipInfo> info;
 
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
@@ -19,18 +23,20 @@ public class Deduce extends AbstractEasyCard {
 
     private static final int COST = 1;
     private static final int BLK = 7;
-    private static final int UP_BLK = 2;
-    private static final int SCRY = 3;
-    private static final int UP_SCRY = 1;
+    private static final int UP_BLK = 3;
+    private static final int AMT = 1;
+    private static final int UP_AMT = 1;
 
-    public Deduce() {
+    public StarChart() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseBlock = block = BLK;
-        baseMagicNumber = magicNumber = SCRY;
+        baseMagicNumber = magicNumber = AMT;
+        cardsToPreview = new StarShard();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
+        Wiz.makeInHand(cardsToPreview, magicNumber);
         /*Wiz.atb(new EnvisionAction(magicNumber, 1, cards -> {
             for (AbstractCard c : cards) {
                 if (c.canUpgrade()) {
@@ -39,13 +45,22 @@ public class Deduce extends AbstractEasyCard {
             }
 
         }));*/
-        Wiz.atb(new ScryAction(magicNumber));
-        Wiz.atb(new DrawCardAction(1));
+        /*Wiz.atb(new ScryAction(magicNumber));
+        Wiz.atb(new DrawCardAction(1));*/
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltips() {
+        if (info == null) {
+            info = new ArrayList<>();
+            info.add(new TooltipInfo(ResonantDamage.STRINGS.NAME, ResonantDamage.STRINGS.DESCRIPTION));
+        }
+        return info;
     }
 
     public void upp() {
         upgradeBlock(UP_BLK);
-        upgradeMagicNumber(UP_SCRY);
+        //upgradeMagicNumber(UP_AMT);
     }
 
     @Override
