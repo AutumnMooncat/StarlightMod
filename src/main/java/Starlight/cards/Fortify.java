f82536ec7f6304ecbe7ddac96a7e792caaa503ba
Alison Turner <alisonmoons@gmail.com>
2022-09-29 19:09:49 -0300

    Fortify now Resonant and Retains

--- src/main/java/Starlight/cards/Fortify.java
+++ src/main/java/Starlight/cards/Fortify.java
@@ -1,9 +1,11 @@
 package Starlight.cards;
 
+import Starlight.cardmods.ResonantMod;
 import Starlight.cards.abstracts.AbstractEasyCard;
 import Starlight.cards.interfaces.TagTeamCard;
 import Starlight.util.CardArtRoller;
 import Starlight.util.Wiz;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.blue.Stack;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -19,8 +21,8 @@ public class Fortify extends AbstractEasyCard {
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
-    private static final int BLK = 8;
-    private static final int UP_BLK = 3;
+    private static final int BLK = 6;
+    private static final int UP_BLK = 2;
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
@@ -28,6 +30,8 @@ public class Fortify extends AbstractEasyCard {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseBlock = block = BLK;
         baseMagicNumber = magicNumber = EFFECT;
+        selfRetain = true;
+        CardModifierManager.addModifier(this, new ResonantMod(true));
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -36,7 +40,7 @@ public class Fortify extends AbstractEasyCard {
     }
 
     public void upp() {
-        //upgradeBlock(UP_BLK);
+        upgradeBlock(UP_BLK);
         upgradeMagicNumber(UP_EFFECT);
     }
 
