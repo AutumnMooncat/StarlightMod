2ebaeca6e826edce4e97c44e8070e6358d203664
Alison Turner <alisonmoons@gmail.com>
2022-09-30 07:53:03 -0300

    Buff Powerfist back to 8(10) damage from 6(8), raising effective damage from 8(11) to 10(13)

--- src/main/java/Starlight/cards/PowerFist.java
+++ src/main/java/Starlight/cards/PowerFist.java
@@ -22,7 +22,7 @@ public class PowerFist extends AbstractEasyCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
-    private static final int DMG = 6;
+    private static final int DMG = 8;
     private static final int UP_DMG = 2;
     private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
