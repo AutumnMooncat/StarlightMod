bc2fc6e4fe4a799bbb9d43b401d67fd959f7ae13
Alison Turner <alisonmoons@gmail.com>
2022-10-02 16:51:30 -0300

    Blazing now Exhausts up to 2(3) cards

--- src/main/java/Starlight/cards/Blazing.java
+++ src/main/java/Starlight/cards/Blazing.java
@@ -2,18 +2,13 @@ package Starlight.cards;
 
 import Starlight.actions.ExhaustByPredAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.powers.ProvidencePower;
 import Starlight.powers.SpellPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.purple.Perseverance;
 import com.megacrit.cardcrawl.cards.purple.WreathOfFlame;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.EnergizedBluePower;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -25,8 +20,8 @@ public class Blazing extends AbstractEasyCard {
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
-    private static final int EFFECT = 5;
-    private static final int UP_EFFECT = 2;
+    private static final int EFFECT = 2;
+    private static final int UP_EFFECT = 1;
 
     public Blazing() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -34,16 +29,19 @@ public class Blazing extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new ExhaustByPredAction(p.hand, 1, c -> true, new AbstractGameAction() {
+        Wiz.atb(new ExhaustByPredAction(p.hand, magicNumber, true, c -> true, new AbstractGameAction() {
             @Override
             public void update() {
-                for (AbstractCard card : ExhaustByPredAction.exhaustedCards) {
+                if (!ExhaustByPredAction.exhaustedCards.isEmpty()) {
+                    Wiz.applyToSelfTop(new SpellPower(p, magicNumber * ExhaustByPredAction.exhaustedCards.size()));
+                }
+                /*for (AbstractCard card : ExhaustByPredAction.exhaustedCards) {
                     if (card.costForTurn == -1) {
                         Wiz.applyToSelf(new SpellPower(p, magicNumber * EnergyPanel.getCurrentEnergy()));
                     } else if (card.costForTurn > 0) {
                         Wiz.applyToSelf(new SpellPower(p, magicNumber * card.costForTurn));
                     }
-                }
+                }*/
                 this.isDone = true;
             }
         }));
@@ -60,6 +58,6 @@ public class Blazing extends AbstractEasyCard {
 
     @Override
     public CardArtRoller.ReskinInfo reskinInfo(String ID) {
-        return new CardArtRoller.ReskinInfo(ID, 0.2f, 0.5f, 0.4f, 0.5f, false);
+        return new CardArtRoller.ReskinInfo(ID, 0.2f, 0.5f, 0.5f, 0.5f, false);
     }
 }
