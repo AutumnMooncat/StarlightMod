adbe12c85e2c2a529dcefbf39ad7120866786480
Alison Turner <alisonmoons@gmail.com>
2022-10-02 16:53:20 -0300

    Added UI strings and proper check for any number of exhausts

--- src/main/java/Starlight/actions/ExhaustByPredAction.java
+++ src/main/java/Starlight/actions/ExhaustByPredAction.java
@@ -19,24 +19,30 @@ public class ExhaustByPredAction extends AbstractGameAction {
     private final CardGroup cardGroup;
     private final Predicate<AbstractCard> filter;
     private final AbstractGameAction followUpAction;
+    private final boolean anyNumber;
     public static final ArrayList<AbstractCard> exhaustedCards = new ArrayList<>();
 
     public ExhaustByPredAction(CardGroup cardGroup) {
-        this(cardGroup, 1, c -> true, null);
+        this(cardGroup, 1, false, c -> true, null);
     }
 
     public ExhaustByPredAction(CardGroup cardGroup, int amount) {
-        this(cardGroup, amount, c -> true, null);
+        this(cardGroup, amount, false, c -> true, null);
     }
 
     public ExhaustByPredAction(CardGroup cardGroup, int amount, Predicate<AbstractCard> filter) {
-        this(cardGroup, amount, filter, null);
+        this(cardGroup, amount, false, filter, null);
     }
 
     public ExhaustByPredAction(CardGroup cardGroup, int amount, Predicate<AbstractCard> filter, AbstractGameAction followUpAction) {
+        this(cardGroup, amount, false, filter, followUpAction);
+    }
+
+    public ExhaustByPredAction(CardGroup cardGroup, int amount, boolean anyNumber, Predicate<AbstractCard> filter, AbstractGameAction followUpAction) {
         this.cardGroup = cardGroup;
         this.amount = amount;
         this.filter = filter;
+        this.anyNumber = anyNumber;
         this.actionType = ActionType.CARD_MANIPULATION;
         this.duration = DUR;
         this.followUpAction = followUpAction;
@@ -59,7 +65,7 @@ public class ExhaustByPredAction extends AbstractGameAction {
             return;
         }
 
-        if (amount >= validCards.size()) {
+        if (amount >= validCards.size() && !anyNumber) {
             for (AbstractCard card : validCards) {
                 cardGroup.moveToExhaustPile(card);
                 exhaustedCards.add(card);
@@ -75,7 +81,22 @@ public class ExhaustByPredAction extends AbstractGameAction {
                 copyMap.put(copy, c);
                 selection.add(copy);
             }
-            Wiz.att(new BetterSelectCardsCenteredAction(selection, this.amount, amount == 1 ? TEXT[1] : TEXT[2] + amount + TEXT[3], cards -> {
+            String displayText;
+            if (anyNumber) {
+                if (amount == 1) {
+                    displayText = TEXT[4] + amount + TEXT[5];
+                } else {
+                    displayText = TEXT[4] + amount + TEXT[6];
+                }
+            } else {
+                if (amount == 1) {
+                    displayText = TEXT[1];
+                } else {
+                    displayText = TEXT[2] + amount + TEXT[3];
+                }
+            }
+            //Pre-filtered so just pass c -> true
+            Wiz.att(new BetterSelectCardsCenteredAction(selection, this.amount, displayText, anyNumber, c -> true, cards -> {
                 for (AbstractCard copy : cards) {
                     AbstractCard c = copyMap.get(copy);
                     cardGroup.moveToExhaustPile(c);
