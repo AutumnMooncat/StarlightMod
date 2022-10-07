b438df4955923101e62eede8632d14923651cc7c
Alison Turner <alisonmoons@gmail.com>
2022-10-06 21:31:13 -0300

    Fortify now boosts Block while in hand. Culminate now boosts damage while in hand.

--- src/main/java/Starlight/cards/Culminate.java
+++ src/main/java/Starlight/cards/Culminate.java
@@ -1,50 +1,75 @@
 package Starlight.cards;
 
-import Starlight.actions.BoostDamageInHandAction;
-import Starlight.actions.ScryFollowUpAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.powers.ProvidencePower;
-import Starlight.powers.SpellPower;
+import Starlight.cards.interfaces.InHandCard;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.blue.Aggregate;
-import com.megacrit.cardcrawl.cards.purple.EmptyFist;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Starlight.TheStarlightMod.makeID;
 
-public class Culminate extends AbstractEasyCard {
+public class Culminate extends AbstractEasyCard implements InHandCard {
     public final static String ID = makeID(Culminate.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 1;
+    private static final int COST = -2;
     private static final int EFFECT = 4;
     private static final int UP_EFFECT = 2;
-    private static final int SP = 2;
+    private static final int USES = 4;
 
     public Culminate() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseMagicNumber = magicNumber = EFFECT;
-        //baseSecondMagic = secondMagic = SP;
+        baseSecondMagic = secondMagic = USES;
+        selfRetain = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new BoostDamageInHandAction(magicNumber, c -> c.type == CardType.ATTACK));
-        /*Wiz.atb(new ScryAction(magicNumber));
-        Wiz.atb(new ScryFollowUpAction(cards -> {
-            if (cards.size() > 0) {
-                Wiz.applyToSelf(new SpellPower(p, secondMagic * cards.size()));
-            }
-        }));*/
+        //Wiz.atb(new BoostDamageInHandAction(magicNumber, c -> c.type == CardType.ATTACK));
+    }
+
+    @Override
+    public float atDamageGive(float dmg, DamageInfo.DamageType type, AbstractCreature target, AbstractCard card) {
+        return dmg + magicNumber;
+    }
+
+    @Override
+    public void onCardUsed(AbstractCard card) {
+        if (card.baseDamage > 0 && secondMagic > 0) {
+            secondMagic--;
+            isSecondMagicModified = baseSecondMagic != secondMagic;
+            flash();
+        }
+        if (secondMagic <= 0) {
+            Wiz.atb(new DiscardSpecificCardAction(this, Wiz.adp().hand));
+        }
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
     }
 
     public void upp() {
         upgradeMagicNumber(UP_EFFECT);
+    }
+
+    @Override
+    public void resetAttributes() {
+        int current = secondMagic;
+        super.resetAttributes();
+        if (current > 0) {
+            secondMagic = current;
+            isSecondMagicModified = secondMagic != baseSecondMagic;
+        }
     }
 
     @Override
--- src/main/java/Starlight/cards/Fortify.java
+++ src/main/java/Starlight/cards/Fortify.java
@@ -1,49 +1,79 @@
 package Starlight.cards;
 
-import Starlight.cardmods.ResonantMod;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.cards.interfaces.TagTeamCard;
+import Starlight.cards.interfaces.InHandCard;
 import Starlight.util.CardArtRoller;
 import Starlight.util.Wiz;
-import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.blue.Stack;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Starlight.TheStarlightMod.makeID;
 
-public class Fortify extends AbstractEasyCard {
+public class Fortify extends AbstractEasyCard implements InHandCard {
     public final static String ID = makeID(Fortify.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.NONE;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 1;
-    private static final int BLK = 4;
-    private static final int UP_BLK = 2;
-    private static final int EFFECT = 2;
+    private static final int COST = -2;
+    private static final int USES = 3;
+    private static final int EFFECT = 3;
     private static final int UP_EFFECT = 1;
 
     public Fortify() {
         super(ID, COST, TYPE, RARITY, TARGET);
-        baseBlock = block = BLK;
+        baseSecondMagic = secondMagic = USES;
         baseMagicNumber = magicNumber = EFFECT;
         selfRetain = true;
         //CardModifierManager.addModifier(this, new ResonantMod(true));
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0 ; i < magicNumber ; i++) {
+        /*for (int i = 0 ; i < magicNumber ; i++) {
             blck();
-        }
+        }*/
         //Wiz.atb(new DrawCardAction(magicNumber));
+    }
+
+    @Override
+    public float onModifyBlock(float blk, AbstractCard card) {
+        return blk + magicNumber;
+    }
+
+    @Override
+    public void onCardUsed(AbstractCard card) {
+        if (card.baseBlock > 0 && secondMagic > 0) {
+            secondMagic--;
+            isSecondMagicModified = baseSecondMagic != secondMagic;
+            flash();
+        }
+        if (secondMagic <= 0) {
+            Wiz.atb(new DiscardSpecificCardAction(this, Wiz.adp().hand));
+        }
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
     }
 
     public void upp() {
         //upgradeBlock(UP_BLK);
         upgradeMagicNumber(UP_EFFECT);
+    }
+
+    @Override
+    public void resetAttributes() {
+        int current = secondMagic;
+        super.resetAttributes();
+        if (current > 0) {
+            secondMagic = current;
+            isSecondMagicModified = secondMagic != baseSecondMagic;
+        }
     }
 
     /*@Override
