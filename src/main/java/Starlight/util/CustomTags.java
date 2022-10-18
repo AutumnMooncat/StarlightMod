018e1592fa980125b82016fe883e8ed2f80730e6
Alison Turner <alisonmoons@gmail.com>
2022-10-18 15:42:07 -0300

    Add a bunch of spellbook tags.

--- src/main/java/Starlight/util/CustomTags.java
+++ src/main/java/Starlight/util/CustomTags.java
@@ -8,4 +8,28 @@ public class CustomTags {
     public static AbstractCard.CardTags STARLIGHT_CAST_ANIM;
     @SpireEnum
     public static AbstractCard.CardTags STARLIGHT_ASCENDED;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_FIRE;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_WATER;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_ICE;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_NATURE;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_DARK;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_LIGHT;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_EARTH;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_STORM;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_SPACE;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_TIME;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_VOID;
+    @SpireEnum
+    public static AbstractCard.CardTags STARLIGHT_SAND;
 }
