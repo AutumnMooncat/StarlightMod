ef08a5da5ead7e1b2472294c8addfa6f417410d2
Alison Turner <alisonmoons@gmail.com>
2022-10-27 00:53:24 -0300

    Add HealthBarRender interface to Burn Power

--- src/main/java/Starlight/powers/BurnPower.java
+++ src/main/java/Starlight/powers/BurnPower.java
@@ -2,6 +2,8 @@ package Starlight.powers;
 
 import Starlight.TheStarlightMod;
 import Starlight.util.Wiz;
+import com.badlogic.gdx.graphics.Color;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
@@ -13,15 +15,19 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import com.megacrit.cardcrawl.powers.IntangiblePower;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
-public class BurnPower extends AbstractPower {
+public class BurnPower extends AbstractPower implements HealthBarRenderPower {
 
     public static final String POWER_ID = TheStarlightMod.makeID(BurnPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
     public final AbstractCreature source;
+
+    private final Color hpBarColor = new Color(-2686721);
 
     public BurnPower(AbstractCreature owner, AbstractCreature source, int amount) {
         this.ID = POWER_ID;
@@ -44,5 +50,18 @@ public class BurnPower extends AbstractPower {
     @Override
     public void updateDescription() {
         this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public int getHealthBarAmount() {
+        if (owner.hasPower(IntangiblePower.POWER_ID) || owner.hasPower(IntangiblePlayerPower.POWER_ID)) {
+            return 1;
+        }
+        return amount;
+    }
+
+    @Override
+    public Color getColor() {
+        return hpBarColor;
     }
 }
