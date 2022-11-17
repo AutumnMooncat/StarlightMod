da712c411735b661a2ea64991b829ba61244dc5e
Alison Turner <alisonmoons@gmail.com>
2022-11-17 16:32:20 -0400

    Flamethrower now deals 8(10) damage and applies Burn equal to unblocked damage dealt.

--- src/main/java/Starlight/cards/bookOfFire/Flamethrower.java
+++ src/main/java/Starlight/cards/bookOfFire/Flamethrower.java
@@ -1,16 +1,22 @@
 package Starlight.cards.bookOfFire;
 
+import Starlight.actions.DamageFollowupAction;
 import Starlight.cards.abstracts.AbstractMagickCard;
 import Starlight.powers.BurnPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.purple.Eruption;
 import com.megacrit.cardcrawl.cards.purple.ReachHeaven;
 import com.megacrit.cardcrawl.cards.tempCards.Expunger;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -22,23 +28,34 @@ public class Flamethrower extends AbstractMagickCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
-    private static final int DMG = 4;
-    private static final int UP_DMG = 1;
+    private static final int DMG = 8;
+    private static final int UP_DMG = 2;
     private static final int EFFECT = 4;
     private static final int UP_EFFECT = 1;
 
     public Flamethrower() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
-        baseMagicNumber = magicNumber = EFFECT;
+        //baseMagicNumber = magicNumber = EFFECT;
         exhaust = true;
         tags.add(CustomTags.STARLIGHT_FIRE);
+        tags.add(CustomTags.STARLIGHT_APPLIES_BURN);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0 ; i < magicNumber ; i++) {
-            dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        }
+        Wiz.atb(new SFXAction("ATTACK_FIRE"));
+        Wiz.atb(new VFXAction(new FlashAtkImgEffect(m.hb.cX, m.hb.cY + 20f * Settings.scale, AbstractGameAction.AttackEffect.FIRE, true), 0.2f));
+        Wiz.atb(new SFXAction("ATTACK_FIRE"));
+        Wiz.atb(new VFXAction(new FlashAtkImgEffect(m.hb.cX + 20f * Settings.scale, m.hb.cY, AbstractGameAction.AttackEffect.FIRE, true), 0.2f));
+        Wiz.atb(new SFXAction("ATTACK_FIRE"));
+        Wiz.atb(new VFXAction(new FlashAtkImgEffect(m.hb.cX, m.hb.cY - 20f * Settings.scale, AbstractGameAction.AttackEffect.FIRE, true), 0.2f));
+        Wiz.atb(new SFXAction("ATTACK_FIRE"));
+        Wiz.atb(new VFXAction(new FlashAtkImgEffect(m.hb.cX - 20f * Settings.scale, m.hb.cY, AbstractGameAction.AttackEffect.FIRE, true), 0.0f));
+        Wiz.atb(new DamageFollowupAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, i -> {
+            if (i > 0) {
+                Wiz.applyToEnemy(m, new BurnPower(m, p, i));
+            }
+        }));
     }
 
     public void upp() {
