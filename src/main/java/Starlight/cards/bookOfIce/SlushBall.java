15d3b5333951a19c8a4dba05c755ee60bd061174
Alison Turner <alisonmoons@gmail.com>
2022-12-10 13:33:49 -0400

    Slush Ball damage restored 5 -> 7(9). Now moved to Uncommon

--- src/main/java/Starlight/cards/bookOfIce/SlushBall.java
+++ src/main/java/Starlight/cards/bookOfIce/SlushBall.java
@@ -7,9 +7,7 @@ import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.blue.Blizzard;
 import com.megacrit.cardcrawl.cards.blue.MultiCast;
-import com.megacrit.cardcrawl.cards.purple.Meditate;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -18,12 +16,12 @@ import static Starlight.TheStarlightMod.makeID;
 public class SlushBall extends AbstractMagickCard {
     public final static String ID = makeID(SlushBall.class.getSimpleName());
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 1;
-    private static final int DMG = 5;
+    private static final int DMG = 7;
     private static final int UP_DMG = 2;
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
@@ -43,7 +41,7 @@ public class SlushBall extends AbstractMagickCard {
     }
 
     public void upp() {
-        //upgradeDamage(UP_DMG);
+        upgradeDamage(UP_DMG);
         upgradeMagicNumber(UP_EFFECT);
     }
 
