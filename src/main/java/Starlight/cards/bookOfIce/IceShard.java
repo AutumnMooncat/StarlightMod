9274ace69660fcb681024d8b057f9439865b2b9a
Alison Turner <alisonmoons@gmail.com>
2022-12-11 14:38:30 -0400

    Ice Shard moved to Common, damage lowered 7(10) -> 6(9).

--- src/main/java/Starlight/cards/bookOfIce/IceShard.java
+++ src/main/java/Starlight/cards/bookOfIce/IceShard.java
@@ -1,18 +1,11 @@
 package Starlight.cards.bookOfIce;
 
-import Starlight.actions.MoveFromDrawToHandAction;
 import Starlight.cards.abstracts.AbstractMagickCard;
-import Starlight.cards.interfaces.OnEnterDrawPileCard;
-import Starlight.powers.ChillPower;
-import Starlight.powers.WetPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.blue.Blizzard;
-import com.megacrit.cardcrawl.cards.blue.ColdSnap;
-import com.megacrit.cardcrawl.cards.colorless.SwiftStrike;
-import com.megacrit.cardcrawl.cards.green.Skewer;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.WeakPower;
@@ -22,12 +15,12 @@ import static Starlight.TheStarlightMod.makeID;
 public class IceShard extends AbstractMagickCard {
     public final static String ID = makeID(IceShard.class.getSimpleName());
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 0;
-    private static final int DMG = 7;
+    private static final int DMG = 6;
     private static final int UP_DMG = 3;
     private static final int EFFECT = 1;
 
