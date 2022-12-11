9db40c8f5136604bd10032468a132d437a1b1017
Alison Turner <alisonmoons@gmail.com>
2022-12-11 14:37:38 -0400

    Frostburn cost lowered 2(1) -> 1(0)

--- src/main/java/Starlight/cards/bookOfIce/Frostburn.java
+++ src/main/java/Starlight/cards/bookOfIce/Frostburn.java
@@ -19,8 +19,8 @@ public class Frostburn extends AbstractMagickCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.POWER;
 
-    private static final int COST = 2;
-    private static final int UP_COST = 1;
+    private static final int COST = 1;
+    private static final int UP_COST = 0;
     private static final int EFFECT = 1;
 
     public Frostburn() {
