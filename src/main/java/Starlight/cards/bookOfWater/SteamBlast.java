7368b9956a6c061aa07b7ed589a465f703036a7f
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-20 02:03:13 -0400

    Steam Blast now deals 5(7) damage for each card in your hand.

--- src/main/java/Starlight/cards/bookOfWater/SteamBlast.java
+++ src/main/java/Starlight/cards/bookOfWater/SteamBlast.java
@@ -1,17 +1,17 @@
 package Starlight.cards.bookOfWater;
 
 import Starlight.cards.abstracts.AbstractMagickCard;
-import Starlight.powers.BurnPower;
-import Starlight.powers.WetPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
-import com.megacrit.cardcrawl.cards.blue.Coolheaded;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.blue.SteamBarrier;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.MindblastEffect;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -23,21 +23,37 @@ public class SteamBlast extends AbstractMagickCard {
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
-    private static final int DMG = 18;
-    private static final int UP_DMG = 6;
+    private static final int DMG = 5;
+    private static final int UP_DMG = 2;
 
     public SteamBlast() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
+        info = baseInfo = 0;
         tags.add(CustomTags.STARLIGHT_WATER);
-        tags.add(CustomTags.STARLIGHT_APPLIES_BURN);
+        //tags.add(CustomTags.STARLIGHT_APPLIES_BURN);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.FIRE);
-        if (m.hasPower(WetPower.POWER_ID)) {
+        this.addToBot(new VFXAction(new MindblastEffect(p.dialogX, p.dialogY, p.flipHorizontal)));
+        Wiz.atb(new DamageAction(m, new DamageInfo(p, info, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, true));
+        /*if (m.hasPower(WetPower.POWER_ID)) {
             Wiz.applyToEnemy(m, new BurnPower(m, p, m.getPower(WetPower.POWER_ID).amount));
-        }
+        }*/
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        info = damage * (Wiz.adp().hand.size()-1);
+        isInfoModified = isDamageModified;
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        info = damage * (Wiz.adp().hand.size()-1);
+        isInfoModified = isDamageModified;
     }
 
     public void upp() {
