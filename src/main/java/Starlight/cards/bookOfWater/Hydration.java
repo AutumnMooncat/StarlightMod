9d1d4a2a9de12bb007e8bcca24fff4370c370825
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-20 02:01:16 -0400

    Hydration now 1 cost and has Quickdraw. Block increased 4(6) -> 5(8).

--- src/main/java/Starlight/cards/bookOfWater/Hydration.java
+++ src/main/java/Starlight/cards/bookOfWater/Hydration.java
@@ -1,12 +1,10 @@
 package Starlight.cards.bookOfWater;
 
 import Starlight.cards.abstracts.AbstractMagickCard;
-import Starlight.powers.WetPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.colorless.Chrysalis;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -20,27 +18,34 @@ public class Hydration extends AbstractMagickCard {
     private static final CardTarget TARGET = CardTarget.ALL;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 0;
-    private static final int BLK = 4;
-    private static final int UP_BLK = 2;
+    private static final int COST = 1;
+    private static final int BLK = 5;
+    private static final int UP_BLK = 3;
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
     public Hydration() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseBlock = block = BLK;
-        baseMagicNumber = magicNumber = EFFECT;
+        //baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_WATER);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new WetPower(mon, magicNumber)));
+        //Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new WetPower(mon, magicNumber)));
+        //Wiz.atb(new DrawCardAction(magicNumber));
+        //Wiz.atb(new DiscardAction(p, p, magicNumber, false));
     }
 
     public void upp() {
         upgradeBlock(UP_BLK);
-        upgradeMagicNumber(UP_EFFECT);
+        //upgradeMagicNumber(UP_EFFECT);
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        Wiz.atb(new DrawCardAction(1));
     }
 
     @Override
