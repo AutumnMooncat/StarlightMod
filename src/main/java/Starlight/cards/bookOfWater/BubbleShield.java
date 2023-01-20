501f8f2d5e397c52b9e2f50fefda4e17f55f5c1a
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-20 03:55:47 -0400

    Bubble Shield now Ethereal and draws 2 cards. Block decreased 9(13) -> 9(12).

--- src/main/java/Starlight/cards/bookOfWater/BubbleShield.java
+++ src/main/java/Starlight/cards/bookOfWater/BubbleShield.java
@@ -4,7 +4,7 @@ import Starlight.cards.abstracts.AbstractMagickCard;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.common.ModifyBlockAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.blue.BootSequence;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -20,11 +20,12 @@ public class BubbleShield extends AbstractMagickCard {
 
     private static final int COST= 1;
     private static final int BLK = 9;
-    private static final int UP_BLK = 4;
-    private static final int EFFECT = 5;
+    private static final int UP_BLK = 3;
+    private static final int EFFECT = 2;
 
     public BubbleShield() {
         super(ID, COST, TYPE, RARITY, TARGET);
+        isEthereal = true;
         baseBlock = block = BLK;
         baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_WATER);
@@ -32,13 +33,8 @@ public class BubbleShield extends AbstractMagickCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
+        Wiz.atb(new DrawCardAction(magicNumber));
         //Wiz.applyToSelf(new BubbleShieldPower(p, magicNumber));
-    }
-
-    //TODO - Gains +1 from Magick Conduit without consuming it. Very slight oversight but worth considering.
-    @Override
-    public void triggerOnManualDiscard() {
-        Wiz.atb(new ModifyBlockAction(this.uuid, magicNumber));
     }
 
     public void upp() {
