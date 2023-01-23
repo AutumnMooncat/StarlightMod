9312eab7c837abca69493f965796639f8ae23bd3
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-22 22:40:06 -0400

    Flow now draws, discards, and swaps.

--- src/main/java/Starlight/cards/bookOfWater/Flow.java
+++ src/main/java/Starlight/cards/bookOfWater/Flow.java
@@ -1,15 +1,16 @@
 package Starlight.cards.bookOfWater;
 
-import Starlight.actions.FlowAction;
+import Starlight.actions.SwapAction;
 import Starlight.cards.abstracts.AbstractMagickCard;
+import Starlight.characters.StarlightSisters;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.purple.Tranquility;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -17,30 +18,40 @@ public class Flow extends AbstractMagickCard {
     public final static String ID = makeID(Flow.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final int DMG = 6;
     private static final int UP_DMG = 3;
+    private static final int EFFECT = 1;
+    private static final int UP_EFFECT = 1;
 
     public Flow() {
         super(ID, COST, TYPE, RARITY, TARGET);
-        baseDamage = damage = DMG;
+        //baseDamage = damage = DMG;
+        baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_WATER);
+        tags.add(CustomTags.STARLIGHT_SWAPS);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        Wiz.atb(new FlowAction(cards -> {
+        //dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        /*Wiz.atb(new FlowAction(cards -> {
             if (!cards.isEmpty()) {
                 Wiz.applyToSelfTop(new DrawCardNextTurnPower(Wiz.adp(), cards.size()));
             }
-        }));
+        }));*/
+        Wiz.atb(new DrawCardAction(magicNumber));
+        Wiz.atb(new DiscardAction(p, p, magicNumber, false));
+        if (p instanceof StarlightSisters) {
+            Wiz.atb(new SwapAction());
+        }
     }
 
     public void upp() {
-        upgradeDamage(UP_DMG);
+        //upgradeDamage(UP_DMG);
+        upgradeMagicNumber(UP_EFFECT);
     }
 
     @Override
