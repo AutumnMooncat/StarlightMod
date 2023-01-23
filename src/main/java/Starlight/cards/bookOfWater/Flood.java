820829e4880ba48ff069d394006baa9aa702c363
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-22 22:44:10 -0400

    Flood cost lowered 2(1) -> 1. Removed Vuln effect. Now upgrades to not exhaust.

--- src/main/java/Starlight/cards/bookOfWater/Flood.java
+++ src/main/java/Starlight/cards/bookOfWater/Flood.java
@@ -3,13 +3,11 @@ package Starlight.cards.bookOfWater;
 import Starlight.cards.abstracts.AbstractMagickCard;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
-import Starlight.util.Wiz;
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.unique.ExpertiseAction;
 import com.megacrit.cardcrawl.cards.purple.Meditate;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -20,30 +18,32 @@ public class Flood extends AbstractMagickCard {
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 2;
+    private static final int COST = 1;
     private static final int UP_COST = 1;
     private static final int EFFECT = 3;
     private static final int UP_EFFECT = 2;
 
     public Flood() {
         super(ID, COST, TYPE, RARITY, TARGET);
-        baseMagicNumber = magicNumber = EFFECT;
+        //baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_WATER);
         exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new ExpertiseAction(p, BaseMod.MAX_HAND_SIZE));
-        Wiz.forAllMonstersLiving(mon -> {
+        /*Wiz.forAllMonstersLiving(mon -> {
             //Wiz.applyToEnemy(mon, new WetPower(mon, magicNumber));
             //Wiz.applyToEnemy(mon, new WeakPower(mon, magicNumber, false));
             Wiz.applyToEnemy(mon, new VulnerablePower(mon, magicNumber, false));
-        });
+        });*/
     }
 
     public void upp() {
         //upgradeMagicNumber(UP_EFFECT);
-        upgradeBaseCost(UP_COST);
+        //upgradeBaseCost(UP_COST);
+        exhaust = false;
+        uDesc();
     }
 
     @Override
