8f9ece86cfaa62cb9fda6871dd39b4c83d6a59e4
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-22 22:45:13 -0400

    Blink cost increased 1 -> 2, Block increased 7(10) -> 8(11), now gives Block next turn instead of draw.

--- src/main/java/Starlight/cards/bookOfTime/Blink.java
+++ src/main/java/Starlight/cards/bookOfTime/Blink.java
@@ -7,7 +7,7 @@ import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.cards.green.DodgeAndRoll;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -18,9 +18,9 @@ public class Blink extends AbstractMagickCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final int UP_COST = 0;
-    private static final int BLK = 7;
+    private static final int BLK = 8;
     private static final int UP_BLK = 3;
     private static final int EFFECT = 2;
     private static final int UP_EFFECT = 1;
@@ -28,13 +28,14 @@ public class Blink extends AbstractMagickCard {
     public Blink() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseBlock = block = BLK;
-        baseMagicNumber = magicNumber = EFFECT;
+        //baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_TIME);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        Wiz.applyToSelf(new DrawCardNextTurnPower(p, magicNumber));
+        //Wiz.applyToSelf(new DrawCardNextTurnPower(p, magicNumber));
+        Wiz.applyToSelf(new NextTurnBlockPower(p, block));
     }
 
     public void upp() {
