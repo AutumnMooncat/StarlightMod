4e70870bf567961b90c64b62b6161090b9ac602e
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-22 22:40:59 -0400

    Resolve now gives 3(5) Block. No longer upgrades to Foretell 2.

--- src/main/java/Starlight/cards/Resolve.java
+++ src/main/java/Starlight/cards/Resolve.java
@@ -1,10 +1,8 @@
 package Starlight.cards;
 
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.cards.abstracts.AbstractMagickCard;
 import Starlight.powers.ForetellNextCardPower;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.cards.red.Entrench;
 import com.megacrit.cardcrawl.cards.tempCards.Insight;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -19,20 +17,25 @@ public class Resolve extends AbstractEasyCard {
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 0;
+    private static final int BLOCK = 3;
+    private static final int UP_BLOCK = 2;
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
     public Resolve() {
         super(ID, COST, TYPE, RARITY, TARGET);
+        baseBlock = block = BLOCK;
         baseMagicNumber = magicNumber = EFFECT;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
         Wiz.applyToSelf(new ForetellNextCardPower(p, magicNumber));
     }
 
     public void upp() {
-        upgradeMagicNumber(UP_EFFECT);
+        upgradeBlock(UP_BLOCK);
+        //upgradeMagicNumber(UP_EFFECT);
     }
 
     @Override
