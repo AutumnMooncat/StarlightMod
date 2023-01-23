b4c0dff50c2e3d1d698b2cc78828b43e7d32432c
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-23 00:32:39 -0400

    Bubble Shield rework. Block decreased 9(12) -> 7(10), now Innate Exhaust, and provides 1 Artifact.

--- src/main/java/Starlight/cards/bookOfWater/BubbleShield.java
+++ src/main/java/Starlight/cards/bookOfWater/BubbleShield.java
@@ -4,10 +4,10 @@ import Starlight.cards.abstracts.AbstractMagickCard;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.blue.BootSequence;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
 
 import static Starlight.TheStarlightMod.makeID;
 
@@ -19,26 +19,31 @@ public class BubbleShield extends AbstractMagickCard {
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST= 1;
-    private static final int BLK = 9;
+    private static final int BLK = 7;
     private static final int UP_BLK = 3;
-    private static final int EFFECT = 2;
+    private static final int EFFECT = 1;
+    private static final int UP_EFFECT = 1;
 
     public BubbleShield() {
         super(ID, COST, TYPE, RARITY, TARGET);
-        isEthereal = true;
         baseBlock = block = BLK;
         baseMagicNumber = magicNumber = EFFECT;
         tags.add(CustomTags.STARLIGHT_WATER);
+        isInnate = true;
+        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        Wiz.atb(new DrawCardAction(magicNumber));
+        Wiz.applyToSelf(new ArtifactPower(p, magicNumber));
+        //Wiz.atb(new ModifyBlockAction(uuid, magicNumber));
+        //Wiz.atb(new DrawCardAction(magicNumber));
         //Wiz.applyToSelf(new BubbleShieldPower(p, magicNumber));
     }
 
     public void upp() {
         upgradeBlock(UP_BLK);
+        //upgradeMagicNumber(UP_EFFECT);
     }
 
     @Override
