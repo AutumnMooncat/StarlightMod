0bfc6bf31629943188577253cf0b5b874e4a3480
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-01-23 22:57:12 -0400

    Add Float - 1 Cost Common Skill - Gain 7(9) Block and 3(5) SP.

---
+++ src/main/java/Starlight/cards/Float.java
@@ -0,0 +1,51 @@
+package Starlight.cards;
+
+import Starlight.cards.abstracts.AbstractEasyCard;
+import Starlight.powers.SpellPower;
+import Starlight.util.CardArtRoller;
+import Starlight.util.Wiz;
+import com.megacrit.cardcrawl.cards.green.Expertise;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Starlight.TheStarlightMod.makeID;
+
+public class Float extends AbstractEasyCard {
+    public final static String ID = makeID(Float.class.getSimpleName());
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+
+    private static final int COST = 1;
+    private static final int BLK = 7;
+    private static final int UP_BLK = 2;
+    private static final int EFFECT = 3;
+    private static final int UP_EFFECT = 2;
+
+    public Float() {
+        super(ID, COST, TYPE, RARITY, TARGET);
+        baseBlock = block = BLK;
+        baseMagicNumber = magicNumber = EFFECT;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        Wiz.applyToSelf(new SpellPower(p, magicNumber));
+    }
+
+    public void upp() {
+        upgradeBlock(UP_BLK);
+        upgradeMagicNumber(UP_EFFECT);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Expertise.ID;
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.2f, 0.5f, 0.65f, 1, false);
+    }
+}
