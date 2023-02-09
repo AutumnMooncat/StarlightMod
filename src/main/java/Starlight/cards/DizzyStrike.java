81ebe595285cae42d570b2b04260dc597d4c70e3
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-09 13:55:38 -0400

    Dizzy Strike rework - Cost increased 1 -> 2. Hits increased 2 -> 3.

--- src/main/java/Starlight/cards/DizzyStrike.java
+++ src/main/java/Starlight/cards/DizzyStrike.java
@@ -1,8 +1,6 @@
 package Starlight.cards;
 
-import Starlight.actions.SwapAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.characters.StarlightSisters;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -20,29 +18,26 @@ public class DizzyStrike extends AbstractEasyCard {
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final int DMG = 7;
     private static final int UP_DMG = 2;
     private static final int EFFECT = 3;
-    private static final int UP_EFFECT = 2;
+    private static final int UP_EFFECT = 1;
 
     public DizzyStrike() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
+        baseMagicNumber = magicNumber = EFFECT;
         tags.add(CardTags.STRIKE);
         tags.add(CustomTags.STARLIGHT_SWAPS);
         //cardsToPreview = new Dazed();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-        Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-        //dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        //dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        //Wiz.atb(new MakeTempCardInDrawPileAction(cardsToPreview.makeStatEquivalentCopy(), 1, true, true));
-        if (p instanceof StarlightSisters) {
-            Wiz.atb(new SwapAction());
+        for (int i = 0 ; i < magicNumber - 1 ; i ++) {
+            Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         }
+        Wiz.atb(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
     }
 
     public void upp() {
@@ -54,8 +49,4 @@ public class DizzyStrike extends AbstractEasyCard {
         return FlyingSleeves.ID;
     }
 
-    /*@Override
-    public void onTagTrigger(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-    }*/
 }
