5995a9b9419c02219c8051ee65517b3e0e935be2
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-02-09 23:13:34 -0400

    Thought Bloom cost increased 2(1) -> 3(2)

--- src/main/java/Starlight/cards/Thoughtbloom.java
+++ src/main/java/Starlight/cards/Thoughtbloom.java
@@ -16,8 +16,8 @@ public class Thoughtbloom extends AbstractEasyCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.POWER;
 
-    private static final int COST = 2;
-    private static final int UP_COST = 1;
+    private static final int COST = 3;
+    private static final int UP_COST = 2;
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
