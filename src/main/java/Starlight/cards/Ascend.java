38241d91a0c7c4c2aa155e301afbf931c1cc7595
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-03-02 16:34:11 -0400

    Ascend rework - Now 1 cost instead of X cost, can always target Skills, and upgrades to not Exhaust. Now Projects a card and a copy of it immediately.

--- src/main/java/Starlight/cards/Ascend.java
+++ src/main/java/Starlight/cards/Ascend.java
@@ -1,27 +1,17 @@
 package Starlight.cards;
 
-import Starlight.actions.EasyXCostAction;
 import Starlight.actions.ProjectCardsInHandAction;
-import Starlight.cardmods.AscendedMod;
+import Starlight.actions.ProjectSpecificCardAction;
 import Starlight.cards.abstracts.AbstractEasyCard;
-import Starlight.powers.ChillPower;
-import Starlight.powers.ProjectCopyPower;
 import Starlight.util.CardArtRoller;
 import Starlight.util.CustomTags;
 import Starlight.util.Wiz;
-import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.LimitBreak;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
-import com.megacrit.cardcrawl.vfx.combat.BlizzardEffect;
 import com.megacrit.cardcrawl.vfx.combat.InflameEffect;
 
 import static Starlight.TheStarlightMod.makeID;
@@ -33,10 +23,8 @@ public class Ascend extends AbstractEasyCard {
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
 
-    private static final int COST = -1;
+    private static final int COST = 1;
     private static final int UP_COST = 0;
-
-    private boolean allowSkill = false;
 
     public Ascend() {
         super(ID, COST, TYPE, RARITY, TARGET);
@@ -45,36 +33,20 @@ public class Ascend extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.atb(new EasyXCostAction(this, (effect, params) -> {
-            Wiz.att(new ProjectCardsInHandAction(1, c -> c.type == CardType.ATTACK || (allowSkill && c.type == CardType.SKILL), new AbstractGameAction() {
-                @Override
-                public void update() {
-                    for (AbstractCard c : ProjectCardsInHandAction.projectedCards) {
-                        if (effect > 0) {
-                            Wiz.att(new ApplyPowerAction(p, p, new ProjectCopyPower(p, c, effect)));
-                        }
-                        Wiz.att(new VFXAction(p, new InflameEffect(p), 0.3F));
-                    }
-                    this.isDone = true;
-                }
-            }));
-            return true;
-        }));
-
-        /*Wiz.atb(new ProjectCardsInHandAction(1, c -> c.type == CardType.ATTACK || (allowSkill && c.type == CardType.SKILL), new AbstractGameAction() {
+        Wiz.atb(new ProjectCardsInHandAction(1, c -> c.type == CardType.ATTACK || c.type == CardType.SKILL, new AbstractGameAction() {
             @Override
             public void update() {
+                addToTop(new VFXAction(p, new InflameEffect(p), 0.3F));
                 for (AbstractCard c : ProjectCardsInHandAction.projectedCards) {
-                    CardModifierManager.addModifier(c, new AscendedMod());
-                    this.addToBot(new VFXAction(p, new InflameEffect(p), 0.3F));
+                    addToTop(new ProjectSpecificCardAction(c.makeStatEquivalentCopy()));
                 }
                 this.isDone = true;
             }
-        }));*/
+        }));
     }
 
     public void upp() {
-        allowSkill = true;
+        exhaust = false;
         uDesc();
         //upgradeBaseCost(UP_COST);
     }
