7e5bc9d97ce1080b9b56a913fef44e4f971350a0
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-07 23:40:16 -0300

    Muddy Waters - Now called Dredge.

--- src/main/java/Starlight/cards/bookOfWater/Dredge.java
+++ src/main/java/Starlight/cards/bookOfWater/Dredge.java
@@ -12,8 +12,8 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Starlight.TheStarlightMod.makeID;
 
-public class MuddyWaters extends AbstractMagickCard {
-    public final static String ID = makeID(MuddyWaters.class.getSimpleName());
+public class Dredge extends AbstractMagickCard {
+    public final static String ID = makeID(Dredge.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
@@ -25,7 +25,7 @@ public class MuddyWaters extends AbstractMagickCard {
     private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
-    public MuddyWaters() {
+    public Dredge() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
         baseMagicNumber = magicNumber = EFFECT;
@@ -34,9 +34,6 @@ public class MuddyWaters extends AbstractMagickCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        //addToBot(new ApplyPowerActionWithFollowup(new ApplyPowerAction(m, p, new StrengthPower(m, -magicNumber), -magicNumber), new ApplyPowerAction(m, p, new GainStrengthPower(m, magicNumber), magicNumber)));
-        //Wiz.atb(new DrawCardAction(magicNumber));
-        //Wiz.atb(new DiscardAction(p, p, magicNumber, false));
         for (int i = 0 ; i < magicNumber ; i++) {
             Wiz.atb(new RandomCardFromDiscardPileToHandAction());
         }
