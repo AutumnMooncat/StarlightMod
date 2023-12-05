c48b726200beb9253abe6fb34c007e2e5b77cd17
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-04 21:30:15 -0400

    Sticky Sap rework. Now gives 6(8) Block. Tangles lowered 3(4) -> 1(2).

--- src/main/java/Starlight/cards/bookOfNature/StickySap.java
+++ src/main/java/Starlight/cards/bookOfNature/StickySap.java
@@ -15,25 +15,30 @@ public class StickySap extends AbstractMagickCard {
     public final static String ID = makeID(StickySap.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
     private static final CardType TYPE = CardType.SKILL;
 
     private static final int COST = 1;
-    private static final int EFFECT = 3;
+    private static final int BLK = 6;
+    private static final int UP_BLK = 2;
+    private static final int EFFECT = 1;
     private static final int UP_EFFECT = 1;
 
     public StickySap() {
         super(ID, COST, TYPE, RARITY, TARGET);
+        baseBlock = block = BLK;
         baseMagicNumber = magicNumber = EFFECT;
         selfRetain = true;
         tags.add(CustomTags.STARLIGHT_NATURE);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
         Wiz.applyToEnemy(m, new TanglePower(m, magicNumber));
     }
 
     public void upp() {
+        upgradeBlock(UP_BLK);
         upgradeMagicNumber(UP_EFFECT);
     }
 
