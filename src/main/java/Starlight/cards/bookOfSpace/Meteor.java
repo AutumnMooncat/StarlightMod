65759faccc007515a749d960510f04342f824bd7
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-04-05 12:57:32 -0300

    Meteor now AOE

--- src/main/java/Starlight/cards/bookOfSpace/Meteor.java
+++ src/main/java/Starlight/cards/bookOfSpace/Meteor.java
@@ -11,7 +11,6 @@ import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.blue.MeteorStrike;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 import com.megacrit.cardcrawl.vfx.combat.WeightyImpactEffect;
 
 import static Starlight.TheStarlightMod.makeID;
@@ -20,30 +19,26 @@ public class Meteor extends AbstractMagickCard implements OnProjectCard {
     public final static String ID = makeID(Meteor.class.getSimpleName());
 
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
 
     private static final int COST = 2;
     private static final int DMG = 13;
     private static final int UP_DMG = 4;
-    private static final int VULN = 2;
 
     private boolean projected;
 
     public Meteor() {
         super(ID, COST, TYPE, RARITY, TARGET);
         baseDamage = damage = DMG;
-        //baseMagicNumber = magicNumber = VULN;
         tags.add(CustomTags.STARLIGHT_SPACE);
+        isMultiDamage = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (m != null) {
-            this.addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
-        }
+        Wiz.forAllMonstersLiving(mon -> addToBot(new VFXAction(new WeightyImpactEffect(mon.hb.cX, mon.hb.cY))));
         this.addToBot(new WaitAction(0.8F));
-        dmg(m, AbstractGameAction.AttackEffect.NONE);
-        //Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
+        allDmg(AbstractGameAction.AttackEffect.NONE);
         projected = false;
     }
 
