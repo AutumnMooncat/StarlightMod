f5e425d03f6a443934d41da06c9a6850449debe0
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-05-16 19:18:54 -0300

    WallopDamage now adds to top

--- src/main/java/Starlight/damageMods/WallopDamage.java
+++ src/main/java/Starlight/damageMods/WallopDamage.java
@@ -2,17 +2,15 @@ package Starlight.damageMods;
 
 import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class WallopDamage extends AbstractDamageModifier {
 
     @Override
     public void onLastDamageTakenUpdate(DamageInfo info, int lastDamageTaken, int overkillAmount, AbstractCreature target) {
         if (lastDamageTaken > 0) {
-            addToBot(new GainBlockAction(info.owner, lastDamageTaken));
+            addToTop(new GainBlockAction(info.owner, lastDamageTaken));
         }
     }
 
